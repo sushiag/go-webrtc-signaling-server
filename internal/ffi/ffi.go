6e0921a4dd2792b3bc2e1d10afb8febe6836d9c4
shiatomaro <officialtrishiagavile@gmail.com>
2025-03-22 14:13:55 +0800

    changed import

--- internal/ffi/ffi.go
+++ internal/ffi/ffi.go
@@ -6,15 +6,31 @@ package ffi
 #include <stdio.h>
 void StartWebRTC();
 */
-import "C"
 
-import "github.com/pion/webrtc/v4"
+import (
+	"C"
+	"fmt"
+	"log"
+
+	"github.com/pion/webrtc/v4"
+)
 
 var peerConnection *webrtc.PeerConnection
 
 //export StartWebRTC
 func StartWebRTC() {
-	config := webrtc.Configuration{ /* ICE servers config */ }
-	pc, _ := webrtc.NewPeerConnection(config)
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{URLs: []string{"stun:stun.l.google.com:19302"}},
+		},
+	}
+
+	pc, err := webrtc.NewPeerConnection(config)
+	if err != nil {
+		log.Println("[FFI] Failed to create peer connection:", err)
+		return
+	}
 	peerConnection = pc
+
+	fmt.Println("[FFI] WebRTC PeerConnection started successfully")
 }
