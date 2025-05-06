15846c389ff10b5935f5aa6843fc913bbd0a1c82
shiatomaro <officialtrishiagavile@gmail.com>
2025-05-06 16:15:36 +0800

    deleted rougue "w"

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -10,7 +10,6 @@ func main() {
 	server, wsURL := server.StartServer(":8080") // Pass bind address here
 
 	log.Printf("[SERVER] WebSocket server started at %s", wsURL)
-	W
 	defer func() {
 		if err := server.Close(); err != nil {
 			log.Fatalf("[SERVER] Failed to stop the server: %v", err)
