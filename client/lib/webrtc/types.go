195a4ff3c66901ed6b49696f68c01eb6f311dea9
shiatomaro <officialtrishiagavile@gmail.com>
2025-06-24 13:22:02 +0800

    deleted local enum

--- client/lib/webrtc/types.go
+++ client/lib/webrtc/types.go
@@ -2,9 +2,9 @@ package webrtc
 
 import (
 	"context"
-	"encoding/json"
 
 	"github.com/pion/webrtc/v4"
+	"github.com/sushiag/go-webrtc-signaling-server/client/lib/common"
 )
 
 type Peer struct {
@@ -34,96 +34,19 @@ type PeerManager struct {
 }
 
 type SignalingMessage struct {
-	Type      MessageType `json:"type"`
-	Content   string      `json:"content,omitempty"`
-	RoomID    uint64      `json:"room_id,omitempty"`
-	Sender    uint64      `json:"from,omitempty"`
-	Target    uint64      `json:"to,omitempty"`
-	Candidate string      `json:"candidate,omitempty"`
-	SDP       string      `json:"sdp,omitempty"`
-	Users     []uint64    `json:"users,omitempty"`
-	Text      string      `json:"text,omitempty"`
-	Payload   Payload     `json:"payload,omitempty"`
+	Type      common.MessageType `json:"type"`
+	Content   string             `json:"content,omitempty"`
+	RoomID    uint64             `json:"room_id,omitempty"`
+	Sender    uint64             `json:"from,omitempty"`
+	Target    uint64             `json:"to,omitempty"`
+	Candidate string             `json:"candidate,omitempty"`
+	SDP       string             `json:"sdp,omitempty"`
+	Users     []uint64           `json:"users,omitempty"`
+	Text      string             `json:"text,omitempty"`
+	Payload   Payload            `json:"payload,omitempty"`
 }
 
 type Payload struct {
 	DataType string `json:"data_type"`
 	Data     []byte `json:"data"`
-}
-
-type MessageType int
-
-const (
-	MessageTypeOffer MessageType = iota
-	MessageTypeAnswer
-	MessageTypeICECandidate
-	MessageTypePeerJoined
-	MessageTypeDisconnect
-	MessageTypeSendMessage
-	MessageTypePeerList
-	MessageTypeHostChanged
-	MessageTypeStartSession
-)
-
-func (mt MessageType) String() string {
-	switch mt {
-	case MessageTypeOffer:
-		return "offer"
-	case MessageTypeAnswer:
-		return "answer"
-	case MessageTypeICECandidate:
-		return "ice-candidate"
-	case MessageTypePeerJoined:
-		return "peer-joined"
-	case MessageTypeDisconnect:
-		return "disconnect"
-	case MessageTypeSendMessage:
-		return "send-message"
-	case MessageTypePeerList:
-		return "peer-list"
-	case MessageTypeHostChanged:
-		return "host-changed"
-	case MessageTypeStartSession:
-		return "start-session"
-	default:
-		return "unknown"
-	}
-}
-
-func (mt *MessageType) UnmarshalJSON(b []byte) error {
-	var s string
-	if err := json.Unmarshal(b, &s); err != nil {
-		return err
-	}
-	*mt = ParseMessageType(s)
-	return nil
-}
-
-func (mt MessageType) MarshalJSON() ([]byte, error) {
-	return json.Marshal(mt.String())
-}
-
-func ParseMessageType(s string) MessageType {
-	switch s {
-	case "offer":
-		return MessageTypeOffer
-	case "answer":
-		return MessageTypeAnswer
-	case "ice-candidate":
-		return MessageTypeICECandidate
-	case "peer-joined":
-		return MessageTypePeerJoined
-	case "disconnect":
-		return MessageTypeDisconnect
-	case "send-message":
-		return MessageTypeSendMessage
-	case "peer-list":
-		return MessageTypePeerList
-	case "host-changed":
-		return MessageTypeHostChanged
-	case "start-session":
-		return MessageTypeStartSession
-	default:
-		return -1
-	}
 }
