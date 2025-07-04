1e3a9455540f2b57a121069f06f1ba180a12bb4c
shiatomaro <officialtrishiagavile@gmail.com>
2025-07-04 15:35:17 +0800

    sqlc drama

---
+++ server/lib/db/query.sql
@@ -0,0 +1,16 @@
+-- name: CreateUser :exec
+INSERT INTO users (username, password)
+VALUES (?, ?);
+
+-- name: UpdateUserPassword :exec
+UPDATE users
+SET password = ?
+WHERE username = ?
+AND password = ?;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = ?
+
+-- name: GetUserByUsername :one
+SELECT * FROM users WHERE username = ?;
